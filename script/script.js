09da650b2fd2e7cf61a06c58a3890f8f697dd97c
Dima Vorontcov <dv.notam@yandex.ru>
2020-11-09 01:32:32 +0300

    Первая итерация

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 let popup = document.querySelector('.popup');
 let popupCloseButton = document.querySelector('.popup__close');
-let editButton = document.querySelector('.edit-button');
+let editButton = document.querySelector('.profile__edit-button');
 
 function showPopup() {
     popup.classList.add('popup__open');
@@ -12,3 +12,24 @@ function closePopup() {
 
 editButton.addEventListener('click', showPopup);
 popupCloseButton.addEventListener('click', closePopup);
+
+
+let formElementTitle = document.querySelector('.profile__title');
+let formElementSubtitle = document.querySelector('.profile__subtitle');
+let addButton = document.querySelector('.popup__button');
+let formElement = document.getElementById('form');
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+
+    let nameInput = document.getElementById('name');
+    let jobInput = document.getElementById('job');
+
+    formElementTitle.textContent = nameInput.value;
+    formElementSubtitle.textContent = jobInput.value;
+
+    console.log(evt);
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
+
